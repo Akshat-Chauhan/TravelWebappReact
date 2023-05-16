c23ba0c0aaf0cd22926c71b1616788eb1b003020
Akshat Chauhan <akshat.chauhan1989@gmail.com>
2023-05-16 15:59:51 +0200

    Finished navbar component with css for desktop and mobile version

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,28 @@
 import { Component } from "react";
 import "./navbarstyles.css";
 import { MenuItems } from "./MenuItems";
+import { Link } from "react-router-dom";
 //import {Link} from "react-router-dom"
 
 class Navbar extends Component{
+    state = {clicked: false};
+    handleClick = () =>{
+        this.setState({clicked: !this.state.clicked})
+    }
     render(){
         return(
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">Trivago</h1>
-                <ul className="nav-menu">
+                <div className="menu-icons"  onClick={this.handleClick}>
+                <i className={this.state.clicked ? "fa-solid fa-xmark" : "fa-solid fa-bars"}></i>
+                </div>
+                <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
                     {MenuItems.map((item, index) => {
                         return(
                             <li key={index}>
-                            <a className={item.cName} href="/">
+                            <Link className={item.cName} to={item.url}>
                                 <i className={item.icon}></i>{item.title}
-                            </a>
+                            </Link>
                         </li>
                         )
                     })}
