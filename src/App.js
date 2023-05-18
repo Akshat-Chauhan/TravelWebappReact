f33c3959fdf35ad8710f3bada46e7fb11ee6f55f
Akshat Chauhan <akshat.chauhan1989@gmail.com>
2023-05-18 11:40:08 +0200

    Changes made so that navbar is also usable in small screens

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
-import Navbar from "./components/navbar"
 import { Route, Routes } from 'react-router-dom';
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Service from "./routes/Service";
+import Contact from "./routes/Contact";
+import Navbar from "./components/navbar.js"
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/service" element={<Service />}/>
+        <Route path="/contact" element={<Contact />}/>
+      </Routes>
+
+      <Navbar />  
+
     </div>
   );
 }
